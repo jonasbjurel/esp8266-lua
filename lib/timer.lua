ae05e1279f11ea39fe550b8653ccffc64e45158c
jonasbjurel <jonasbjurel@hotmail.com>
2015-05-27 23:53:35 +0200

    Start of refactoring the platform code: - Smaller footprint - Start of a homogenous build system - More to come soon
    Patch-Set 2
    - Removed tmp files - sorry
    
    Signed-off-by: jonasbjurel <jonasbjurel@hotmail.com>

---
+++ lib/timer.lua
@@ -0,0 +1,76 @@
+-- ##############################################################################
+-- # Copyright (c) 2015 Jonas Bjurel and others as listed below:
+-- # jonas.bjurel@hotmail.com 
+-- #
+-- # All rights reserved. This program and the accompanying materials
+-- # are made available under the terms of the Apache License, Version 2.0
+-- # which accompanies this distribution, and is available at
+-- # http://www.apache.org/licenses/LICENSE-2.0
+-- ##############################################################################
+
+-- ##############################################################################
+-- # Description: An lightweight async timer id helper implementation
+-- # to coordinate the platform an application usage of timer/interupt Id's
+-- # Methods:
+-- # --------
+-- # timer:create_timers() - create a global timer helper context
+-- # Input args: -
+-- # Returns: Global handler for the timer helper
+-- # Description: Creates a global timer helper object
+-- #
+-- # timer:alloc_timer()
+-- # Input args: -
+-- # Returns: Global timer/interupt id 
+-- # Description: Allocates a global timer id. to be used for asynch timer calls 
+-- # like tmr.alarm(timer_id,...)
+-- #
+-- # timer:free_timer(timer_id)
+-- # Input args: timer id
+-- # Returns: 0 if successful, others if not
+-- # Description: De-allocates a timer id previously allocated by timer:alloc_timer.
+-- #
+-- # Usage example:
+-- # alarm=create_timers()
+-- # timer_id=alarm.alloc_timer()
+-- # tmr.alarm(timer_id, timeout, 0 function()
+-- #   print("One and the only timeout for this time, releasing the timer to others")
+-- #   alarm.free_timer(timer_id)
+-- # end)
+-- #
+-- ##############################################################################
+
+-- ##############################################################################
+-- TODO:
+-- ##############################################################################
+
+require "common"
+
+  function create_timers ()
+    --Timer id 2,5 & 6 is used by the system by the system
+    local self = {timers={0,1,2,3,4,5,6}, idle_timers={0,1,2,3,4,5,6}}
+
+    local function alloc_timer ()
+      timer_id=table.remove(self.idle_timers)
+--    DEBUG
+--    print("A new timer id:", timer_id, "is allocated")
+      return timer_id
+    end
+
+    local function free_timer (timer_id)
+--    DEBUG
+--    if (inTable(self.idle_timers, timer_id) == false and inTable(self.timers, timer_id) ~= false) then  
+        table.insert(self.idle_timers,timer_id)
+--      print("A timer id:", timer_id, "is freed")
+        return 0
+--    else
+--      print("PANIC! A timer:", timer_id, "was released while it was already in the idle list or it wasn't a valid timer for this platform:")
+--      print("Rebooting.......")
+--      node.restart()
+--      return nil
+--    end
+--    END DEBUG
+    end
+    
+    return {alloc_timer=alloc_timer,
+            free_timer=free_timer}
+  end
---
+++ lib/user_examples/timer/timer_example.lua
@@ -0,0 +1,47 @@
+require "timer"
+
+  function start_timer (timer)
+    print("Timer id is", timer)
+    tmr.alarm(timer, 1000, 1, function()
+      print("timer id:", timer, "running")
+    end)
+  end
+   
+  start_timers=1
+  timers={}
+  cnt=0
+  alarm=create_timers()
+  timer2=alarm.alloc_timer()
+  print("Starting up all available timers from scratch")
+  tmr.alarm(timer2, 10000, 1, function()
+
+--  DEBUG only working with debug option
+--  if (cnt == 4) then
+--    print("Commiting suicide - releasing a non valid timer 911")
+--    alarm.free_timer(911)
+--  end
+    
+    if (start_timers == 1) then
+      timer1=alarm.alloc_timer()
+      if (timer1 ~= nil) then
+        print("Starting timer", timer1)
+        start_timer(timer1)
+        table.insert(timers, timer1)
+      else
+        print("No more timers available, will start to remove timers")
+        start_timers=0
+      end
+    end
+    if (start_timers == 0) then
+      timer1=table.remove(timers)
+      if (timer1 ~= nil) then
+        print("Stopping timer", timer1)
+        tmr.stop(timer1)
+        alarm.free_timer(timer1)
+      else
+        print("All timers stopped, starting up timers")
+        cnt=cnt+1
+        start_timers=1
+      end
+    end
+  end)
